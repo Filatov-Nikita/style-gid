e64a546d1a92b3d45ab4517f2d9bfa4bbf5362c9
Nikita Antonov <nikita45454@gmail.com>
2025-07-09 17:47:09 +0500

    feat: обновляет вид главного экрана

--- src/components/PromoBanner.vue
+++ src/components/PromoBanner.vue
@@ -1,106 +1,67 @@
 <template>
   <div class="promo-banner">
-    <div class="">
-      <div class="promo-banner__wrap">
-        <div class="promo-banner__left">
-          <h1 class="h1 promo-banner__title">Ваш гид по&nbsp;стилю</h1>
-          <p class="promo-banner__subtitle">Меняйся вместе с&nbsp;Планетой</p>
-          <BaseButton theme="white" @click="scrollToBlock">Записаться</BaseButton>
+    <div class="wrapper">
+      <div class="promo-banner__body">
+        <div class="promo-banner__photo-wrap">
+          <img class="promo-banner__img" src="@/assets/images/main-banner.jpg" width="1922" height="948" alt="Стильная девушка" />
         </div>
-        <div class="promo-banner__right">
-          <img class="promo-banner__img" src="@/assets/images/head-banner.jpg" width="1022" height="878" alt="Стильная девушка" />
+        <div class="promo-banner__content">
+          <div class="promo-banner__content-wrap">
+            <h1 class="promo-banner__title"><span class="tw-uppercase">Шопинг</span><br>со стилистом<br>бесплатно</h1>
+            <div v-if="grid.lg" class="promo-banner__action-wrap">
+              <BaseButton @click="$order.show">Записаться</BaseButton>
+            </div>
+          </div>
         </div>
+      </div>
+      <div v-if="grid.md && !grid.lg" class="promo-banner__action-wrap">
+        <BaseButton @click="$order.show">Записаться</BaseButton>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  function scrollToBlock() {
-    const el = document.querySelector('#order');
-    if(!el) return;
-    el.scrollIntoView({ behavior: 'smooth' });
-  }
+  import useAppGrid from '@/composables/useAppGrid';
+
+  const grid = useAppGrid();
 </script>
 
 <style scoped lang="scss">
   .promo-banner {
-    @apply tw-bg-black;
-
-    &__left {
-      padding: 70px 30px;
-      text-align: center;
-      flex-basis: 750px;
-      flex-grow: 1;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      @apply tw-text-white;
-
-      @include lg {
-        flex-basis: 500px;
-      }
-
-      @include sm {
-        flex-basis: 100%;
-        padding-top: 48px;
-        padding-bottom: 48px;
-      }
+    &__title {
+      font-size: min(2.36vw, 45px);
+      text-transform: uppercase;
+      line-height: 1.15;
+      @apply tw-font-druk tw-font-bold tw-text-white;
     }
 
-    &__right {
-      flex-basis: 681px;
-      flex-grow: 1;
-      max-width: 850px;
-
-      @include lg {
-        flex-basis: 470px;
-      }
-
-      @include sm {
-        flex-basis: 100%;
-      }
+    &__body {
+      position: relative;
     }
 
-    &__wrap {
-      display: flex;
-      justify-content: space-between;
-      max-width: 1920px;
-      margin: 0 auto;
-
-      @include sm {
-        display: block;
-      }
-    }
-
-    &__img {
+    &__content {
+      position: absolute;
+      top: 0;
+      left: 0;
       width: 100%;
       height: 100%;
-      object-fit: cover;
-      max-height: 700px;
-    }
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      z-index: 1;
+      padding-right: 80px;
 
-    &__title {
-      max-width: 441px;
-      margin: 0 auto;
-      margin-bottom: 22px;
-      line-height: 1.15;
-
-      @include sm {
-        max-width: 320px;
-        margin-bottom: 16px;
+      @include md {
+        padding-right: 40px;
       }
     }
 
-    &__subtitle {
-      line-height: 1.35;
-      margin-bottom: 50px;
-      @apply tw-font-light tw-text-24;
+    &__action-wrap {
+      margin-top: 40px;
 
-      @include sm {
-        margin-bottom: 30px;
-        @apply tw-text-18;
+      @include md {
+        margin-top: 30px;
       }
     }
   }
---
+++ src/components/PromoBannerMobile.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="promo-banner">
+    <div class="wrapper">
+      <div class="promo-banner__content">
+        <div class="promo-banner__label">Гид<br/>по стилю</div>
+        <h1 class="promo-banner__title"><span class="tw-uppercase">Шопинг</span><br>со стилистом<br>бесплатно</h1>
+        <div class="promo-banner__action-wrap">
+          <BaseButton class="promo-banner__action" @click="$order.show">Записаться</BaseButton>
+        </div>
+      </div>
+      <div class="promo-banner__photo-wrap">
+        <img class="promo-banner__img" src="@/assets/images/woman.png" width="720" height="666" alt="Стильная девушка" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss">
+  .promo-banner {
+    padding-top: 32px;
+    background: linear-gradient(180deg, #abacae 21.2%, #b9babe 55.46%);
+
+    &__title {
+      font-size: 20px;
+      text-transform: uppercase;
+      line-height: 1.15;
+      margin-bottom: 24px;
+      @apply tw-font-druk tw-font-bold tw-text-white;
+    }
+
+    &__label {
+      text-transform: uppercase;
+      font-size: 34px;
+      line-height: 1.15;
+      margin-bottom: 20px;
+      @apply tw-font-druk tw-font-bold tw-text-black2;
+    }
+
+    &__photo-wrap {
+      margin: 0 -16px;
+    }
+
+    &__img {
+      max-width: 550px;
+      width: 100%;
+    }
+
+    &__content {
+      margin-bottom: 20px;
+    }
+
+    &__action {
+      font-size: 16px !important;
+    }
+  }
+</style>
